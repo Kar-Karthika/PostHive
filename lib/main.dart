f4b883c5c192d7c3054665684408bf9b2c094cee
Karthika V <vkarthika2892@gmail.com>
2025-01-11 20:40:49 +0530

    version1

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:instagram_1/auth/auth_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseAuth.instance.setLanguageCode('en');
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home:
+          const ScreenUtilInit(designSize: Size(365, 566), child: AuthScreen()),
+    );
+  }
+}
